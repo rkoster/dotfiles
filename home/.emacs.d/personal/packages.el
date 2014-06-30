473d63c5c24fde8d2b05e0c148d4de8ff231ef74
Ruben Koster <rkoster@starkandwayne.com>
2014-06-30 12:14:51 +0200

    Go related changes

--- home/.emacs.d/personal/packages.el
+++ home/.emacs.d/personal/packages.el
@@ -4,5 +4,18 @@
 (require 'rvm)
 (add-hook 'ruby-mode-hook
           (lambda () (rvm-activate-corresponding-ruby)))
+
+;; go mode
+(setq load-path (cons "/usr/local/go/misc/emacs" load-path))
+(require 'go-mode-load)
+(setq prelude-whitespace nil)
+(setq tab-width 2)
+(setq-default tab-width 2)
+
+(defun disable-guru-mode ()
+  (guru-mode -1)
+)
+(add-hook 'prelude-prog-mode-hook 'disable-guru-mode t)
+
 (provide 'packages)
 ;;; theme.el ends here
