d53689ac8cb65f6c8c42809d616b263a68a5cfdd
Ruben Koster <rkoster@starkandwayne.com>
2015-04-03 08:53:01 +0200

    Switch to summer theme p2

--- home/.emacs.d/personal/theme.el
+++ home/.emacs.d/personal/theme.el
@@ -1,7 +1,1 @@
-;;; package --- Theme setups the solarized theme
-;;; Code:
-;;;(prelude-require-package 'color-theme-solarized)
-(prelude-require-package 'solarized-theme)
-(load-theme 'solarized-dark t)
-(provide 'theme)
-;;; theme.el ends here
+(global-hl-line-mode 0)
